8290b083064e8e4c2e4c6876c9be9732df58c3ce
Alex-Willenbrink <awillenbrink@gmail.com>
2018-03-29 20:43:59 -0700

    validation added for original sequence

--- src/app/dna-rna-converter-display/dna-rna-converter-display.component.ts
+++ src/app/dna-rna-converter-display/dna-rna-converter-display.component.ts
@@ -1,32 +1,44 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { FormBuilder, FormGroup } from "@angular/forms";
+
+import { SequenceValidator } from "./dna-rna-converter-display.validators";
 
 @Component({
   selector: "dna-rna-converter-display",
   templateUrl: "./dna-rna-converter-display.component.html",
   styleUrls: ["./dna-rna-converter-display.component.css"]
 })
-export class DnaRnaConverterDisplayComponent {
+export class DnaRnaConverterDisplayComponent implements OnInit {
   form: FormGroup;
-  nucleotideConvert: any;
+  nucleotideConvert: any = {};
 
-  constructor(private http: HttpClient, private fb: FormBuilder) {
-    this.http.get("./assets/nucleotide-complements.json").subscribe(data => {
-      this.nucleotideConvert = data;
-    });
+  constructor(private http: HttpClient, private fb: FormBuilder) {}
+
+  ngOnInit() {
     this.form = this.fb.group({
-      original: [""],
+      original: ["", SequenceValidator.createValidator(this.nucleotideConvert)],
       complement: { value: "", disabled: true },
       reverse: { value: "", disabled: true },
       reverseComplement: { value: "", disabled: true }
     });
+
     this.form.get("original").valueChanges.subscribe(sequence => {
+      console.log(this.form.get("original"));
       this.form.get("complement").setValue(this.getComplement(sequence));
       this.form.get("reverse").setValue(this.getReverse(sequence));
       this.form
         .get("reverseComplement")
         .setValue(this.getReverseComplement(sequence));
+    });
+
+    this.http.get("./assets/nucleotide-complements.json").subscribe(data => {
+      this.nucleotideConvert = data;
+      this.form
+        .get("original")
+        .setValidators(
+          SequenceValidator.createValidator(this.nucleotideConvert)
+        );
     });
   }
 
---
+++ src/app/dna-rna-converter-display/dna-rna-converter-display.validators.ts
@@ -0,0 +1,17 @@
+import { AbstractControl } from "@angular/forms";
+
+export class SequenceValidator {
+  static createValidator(nucleotideConvert: any) {
+    return (originalSequenceControl: AbstractControl) => {
+      const sequence = originalSequenceControl.value;
+      const sequenceErrors = [];
+      sequence.split("").forEach(char => {
+        const complementChar = nucleotideConvert[char];
+        if (!complementChar) {
+          sequenceErrors.push(char);
+        }
+      });
+      return sequenceErrors.length > 0 ? { sequenceErrors } : null;
+    };
+  }
+}
