14fcb8c973d44721c7f7aa6f209edd1081814609
Alex-Willenbrink <awillenbrink@gmail.com>
2018-04-16 22:04:10 -0700

    reformatted nucleotide json data and added ability to process to conversion data in sequence service

--- src/app/sequence.service.ts
+++ src/app/sequence.service.ts
@@ -1,17 +1,65 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { forkJoin } from "rxjs/observable/forkJoin";
 
 @Injectable()
 export class SequenceService {
+  nucleotideDnaConvert: any = {};
+  nucleotideRnaConvert: any = {};
   nucleotideConvert: any = {};
+  nucleotideType: string = "DNA";
+
+  nucleotidesDna: any = [];
+  nucleotidesRna: any = [];
+
   loaded: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor(private http: HttpClient) {
-    this.http.get("./assets/nucleotide-complements.json").subscribe(data => {
-      this.nucleotideConvert = data;
+    const dnaNucleotideRequest = this.http.get("./assets/nucleotides-dna.json");
+    const rnaNucleotideRequest = this.http.get("./assets/nucleotides-rna.json");
+    forkJoin(dnaNucleotideRequest, rnaNucleotideRequest).subscribe(data => {
+      this.nucleotidesDna = data[0];
+      this.nucleotideDnaConvert = (data[0] as any).reduce(
+        (nucleotideConvert, nucleotideObj) => {
+          nucleotideConvert[nucleotideObj["Base"]] =
+            nucleotideObj["ComplementBase"];
+          nucleotideConvert[
+            nucleotideObj["Base"].toLowerCase()
+          ] = nucleotideObj["ComplementBase"].toLowerCase();
+          return nucleotideConvert;
+        },
+        {}
+      );
+
+      this.nucleotidesRna = data[1];
+      this.nucleotideRnaConvert = (data[1] as any).reduce(
+        (nucleotideConvert, nucleotideObj) => {
+          nucleotideConvert[nucleotideObj["Base"]] =
+            nucleotideObj["ComplementBase"];
+          nucleotideConvert[
+            nucleotideObj["Base"].toLowerCase()
+          ] = nucleotideObj["ComplementBase"].toLowerCase();
+          return nucleotideConvert;
+        },
+        {}
+      );
+      this.selectNucleotideType(this.nucleotideType);
       this.loaded.next(true);
     });
+  }
+
+  selectNucleotideType(nucleotideType: string): void {
+    switch (nucleotideType) {
+      case "DNA":
+        this.nucleotideType = "DNA";
+        this.nucleotideConvert = this.nucleotideDnaConvert;
+        break;
+      case "RNA":
+        this.nucleotideType = "RNA";
+        this.nucleotideConvert = this.nucleotideRnaConvert;
+        break;
+    }
   }
 
   getComplement(sequence: string): string {
